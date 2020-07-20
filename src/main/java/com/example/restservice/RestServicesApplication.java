30f9c6985a5683c15b7e98d7e2be8799caefbb51
star-001 <fanzhenxin@outlook.com>
2020-07-20 13:27:26 +0800

    Spring Rest Service

---
+++ src/main/java/com/example/restservice/RestServicesApplication.java
@@ -0,0 +1,10 @@
+package com.example.restservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestServicesApplication {
+//    public static void main(String[] args) {
+//    }
+}
